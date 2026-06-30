8df85b11903414b3c9677ea7a8b1eca58214cdb5
Andrey <andrey.alyapkin@ya360.inventive.ru>
2026-06-30 13:47:44 +0300

    Исправления position: fixed;

--- src/components/Header/BasicHeader.vue
+++ src/components/Header/BasicHeader.vue
@@ -11,7 +11,11 @@
 .fixed {
   color: white;
   position: fixed;
+  width: 100%;
+  top: 0;
+  left: 0;
   padding: 10px 20px;
   box-shadow: 0 2px 5px rgba(0, 0, 0, 0.5);
+  z-index: 1000;
 }
 </style>
--- src/components/basic-counter/BasicCounter.vue
+++ src/components/basic-counter/BasicCounter.vue
@@ -24,7 +24,7 @@ function reset() {
 </script>
 
 <template>
-  <h1 style="height: 110vh">Счётчик</h1>
+  <h1 class="Counter">Счётчик</h1>
   <p>Текущее значение: {{ count }}</p>
   <button @click="increment">+1</button>
   <button @click="decrement">-1</button>
@@ -42,5 +42,9 @@ button {
 }
 .reset {
   margin-top: 20px;
+}
+.Counter {
+  height: 110vh;
+  margin-top: 50px;
 }
 </style>
